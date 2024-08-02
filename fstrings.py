24502a355abe849e68b0ba9f0383a3ec08c757e8
gitdoluiscesar <luiscesar33@gmail.com>
2024-08-02 09:09:15 -0300

    teste

---
+++ fstrings.py
@@ -0,0 +1,16 @@
+numeral = input ("digite um numero entre 0 e 9999: ")
+# print ("O número digitado foi: ",(numeral))
+#numeral1 = int(numeral)
+#   if numeral1 > 9999: 
+#  print("numero inválido, digite entre 0 e 9999")
+#len(numeral)
+# print("o numero digitado tem ", len(numeral) , ("digitos"))
+# if len(numeral) == 5: #or len(numeral) < 4:
+#     print(F"{numeral} é um numero invalido")
+# else:
+#     print (F"""
+MILHAR: {numeral[0]}
+CENTENA:{numeral[1]}
+DEZENA: {numeral[2]}
+UNIDADE: 0""")
+print

5c4adf86efabfaa3c468f7631a318074d06f476a
gitdoluiscesar <luiscesar33@gmail.com>
2024-08-02 22:33:05 -0300

    teste1

---
+++ teste.py
@@ -0,0 +1,4 @@
+# n = input('QUAL É SEU NOME?') 
+# print(f'Prazer em te conhecer, {n}!') # sem espaços 
+# print(f'Prazer em te conhecer, {n>20}!') # com 20 espaços
+
---
+++ teste2.py
@@ -0,0 +1,8 @@
+num = input ("numero de 0 a 9999")
+uni = 
+dez = 
+cent =
+mil =
+sist = f"It is very {'0' if num>10 else 'Cheap'}"
+
+print(sist)
